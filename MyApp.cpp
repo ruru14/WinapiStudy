f93dab54875a0d8eb1797987a208d902a57f7497
Sol Lee <ljw2889@gmail.com>
2024-02-19 15:55:18 +0900

    [Update] D2D init

--- MyApp.cpp
+++ MyApp.cpp
@@ -1,7 +1,60 @@
 #include "MyApp.h"
 
+MyApp::MyApp() :
+    myHwnd(nullptr),
+    myDirect2dFactory(nullptr),
+    myRenderTarget(nullptr),
+    myLightSlateGrayBrush(nullptr),
+    myCornflowerBlueBrush(nullptr) {
+}
+
+MyApp::~MyApp() {
+    DiscardDeviceResources();
+    SAFE_RELEASE(myDirect2dFactory);
+}
+
+HRESULT MyApp::CreateDeviceIndependentResources() {
+    HRESULT hr = S_OK;
+    hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, &myDirect2dFactory);
+
+    return hr;
+}
+
+HRESULT MyApp::CreateDeviceResources() {
+    HRESULT hr = S_OK;
+    if (!myRenderTarget) {
+        RECT rc; GetClientRect(myHwnd, &rc);
+        D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+
+        hr = myDirect2dFactory->CreateHwndRenderTarget(
+            D2D1::RenderTargetProperties(),
+            D2D1::HwndRenderTargetProperties(myHwnd, size),
+            &myRenderTarget
+        );
+
+        if (SUCCEEDED(hr)) {
+            hr = myRenderTarget->CreateSolidColorBrush(
+                D2D1::ColorF(D2D1::ColorF::LightSlateGray), &myLightSlateGrayBrush
+            );
+        }
+        if (SUCCEEDED(hr)) {
+            hr = myRenderTarget->CreateSolidColorBrush(
+                D2D1::ColorF(D2D1::ColorF::CornflowerBlue), &myCornflowerBlueBrush
+            );
+        }
+    }
+
+    return hr;
+}
+
+void MyApp::DiscardDeviceResources() {
+    SAFE_RELEASE(myRenderTarget);
+    SAFE_RELEASE(myLightSlateGrayBrush);
+    SAFE_RELEASE(myCornflowerBlueBrush);
+}
+
 HRESULT MyApp::initialize(HINSTANCE hInstance) {
-    HRESULT hr;
+    HRESULT hr = CreateDeviceIndependentResources();
     WNDCLASSEX winCls = { sizeof(WNDCLASSEX) };
     winCls.style = CS_HREDRAW | CS_VREDRAW;
     winCls.lpfnWndProc = MyApp::WndProc;
@@ -22,6 +75,7 @@ HRESULT MyApp::initialize(HINSTANCE hInstance) {
     if (SUCCEEDED(hr)) {
         ShowWindow(myHwnd, SW_SHOWNORMAL);
         UpdateWindow(myHwnd);
+        CreateDeviceResources();
     }
 
     return hr;
@@ -56,10 +110,28 @@ LRESULT MyApp::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 
         if (myApp) {
             switch (message) {
+            case WM_SIZE:
+            {
+            UINT width = LOWORD(lParam);
+            UINT height = HIWORD(lParam);
+            myApp->OnResize(width, height);
+            }
+                result = 0; wasHandled = true;
+                break;
+            case WM_DISPLAYCHANGE:
+                InvalidateRect(hWnd, NULL, FALSE);
+                result = 0; wasHandled = true;
+                break;
+            case WM_PAINT:
+                myApp->OnRender();
+                //ValidateRect(hWnd, NULL);
+                result = 0; wasHandled = true;
+                break;
             case WM_DESTROY:
                 PostQuitMessage(0);
                 result = 1;
                 wasHandled = true;
+                break;
             }
         }
         if (!wasHandled) {
@@ -69,6 +141,57 @@ LRESULT MyApp::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     }
 
     return result;
+}
+
+void MyApp::OnResize(UINT width, UINT height) {
+    if (myRenderTarget) {
+        myRenderTarget->Resize(D2D1::SizeU(width, height));
+    }
+}
+
+HRESULT MyApp::OnRender() {
+    HRESULT hr = S_OK;
+    hr = CreateDeviceResources();
+    if (SUCCEEDED(hr)) {
+        myRenderTarget->BeginDraw();
+        myRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+        myRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+
+        D2D1_SIZE_F rtSize = myRenderTarget->GetSize();
+
+        int width = static_cast<int>(rtSize.width);
+        int height = static_cast<int>(rtSize.height);
+        for (int x = 0; x < width; x += 10) {
+            myRenderTarget->DrawLine(D2D1::Point2F(static_cast<FLOAT>(x), 0.0f),
+                D2D1::Point2F(static_cast<FLOAT>(x), rtSize.height),
+                myLightSlateGrayBrush, 0.5f);
+        }
+        for (int y = 0; y < height; y += 10) {
+            myRenderTarget->DrawLine(D2D1::Point2F(0.0f, static_cast<FLOAT>(y)),
+                D2D1::Point2F(rtSize.width, static_cast<FLOAT>(y)),
+                myLightSlateGrayBrush, 0.5f);
+        }
+
+        D2D1_RECT_F rectangle1 = D2D1::RectF(
+            rtSize.width / 2 - 50.0f, rtSize.height / 2 - 50.0f,
+            rtSize.width / 2 + 50.0f, rtSize.height / 2 + 50.0f
+        );
+        D2D1_RECT_F rectangle2 = D2D1::RectF(
+            rtSize.width / 2 - 100.0f, rtSize.height / 2 - 100.0f,
+            rtSize.width / 2 + 100.0f, rtSize.height / 2 + 100.0f
+        );
+        myRenderTarget->FillRectangle(&rectangle1, myLightSlateGrayBrush);
+        myRenderTarget->DrawRectangle(&rectangle2, myCornflowerBlueBrush);
+
+        hr = myRenderTarget->EndDraw();
+    }
+
+    if (hr == D2DERR_RECREATE_TARGET) {
+        hr = S_OK;
+        DiscardDeviceResources();
+    }
+
+    return hr;
 }
 
 int WinMain(
--- MyApp.h
+++ MyApp.h
@@ -1,15 +1,36 @@
 #pragma once
 
-#include <Windows.h>
+// Default headers
+#include <windows.h>
+#include <tchar.h>
+
+// DirectX headers
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+
+#define SAFE_RELEASE(p) { if(p) { (p)->Release(); (p)=NULL; } }
 
 class MyApp {
 public:
+    MyApp();
+    ~MyApp();
     HRESULT initialize(HINSTANCE hInstance);
     void runMessageLoop();
 
 private:
     static LRESULT WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+    HRESULT CreateDeviceIndependentResources();
+    HRESULT CreateDeviceResources();
+    void DiscardDeviceResources();
+    HRESULT OnRender();
+    void OnResize(UINT width, UINT height);
 
 private:
     HWND myHwnd = nullptr;
+    ID2D1Factory* myDirect2dFactory = nullptr;
+    ID2D1HwndRenderTarget* myRenderTarget = nullptr;
+    ID2D1SolidColorBrush* myLightSlateGrayBrush = nullptr;
+    ID2D1SolidColorBrush* myCornflowerBlueBrush = nullptr;
 };
